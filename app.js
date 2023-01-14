9e4de1ece440267bc6112a0cffc4c102fafb855b
Sargeanthost <sargeanthost@gmail.com>
2023-01-14 18:14:52 -0500

    bump

--- app.js
+++ app.js
@@ -1,47 +1,27 @@
 const express = require('express')
+const app = express();
 const path = require('path');
 const fs = require('fs');
-const mult = require("multiparty");
-const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const port = 3000;
-const uploadDir = `./uploads`;
-const imgBaseUrl = `https://127.0.0.1:${port}/uploads/`;
-var password = fs.readFileSync("./.env.txt", "utf-8");
 
-//static page
-// app.use(express.static(path.join(__dirname, 'static')));
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, '/static/index.html'));
-//   console.log("Home");
-// });
-
-// app.get("/static/spots", (req, res) => {
-//   res.sendFile(path.join(__dirname, '/static/spots.html'));
-//   console.log("spots");
-// });
-
-// app.get("/static/upload", (req, res) => {
-//   res.sendFile(path.join(__dirname, '/static/upload.html'));
-//   console.log("upload");
-// });
+//serve static pages. index and upload will stay as is. spots will be dynamic from server
 app.use(express.static('static'));
+const password = fs.readFileSync("./.env.txt", "utf-8").toString();
 
-app.post("/upload", (req, res) => {
-  let form = new mult.Form({uploadDir: uploadDir})
+mongoose.connect(`mongodb+srv://sampleGoat:${password}@cluster0.6vhvr.mongodb.net/?retryWrites=true&w=majority`)
+// mongoose.connect(`mongodb+srv://sampleGoat:${password}@cluster0.6vhvr.mongodb.net/GoatHacks`)
+const schema = {
+  location: String,
+  area: String,
+  rating: Number,
+  description: String,
+  img: String
+}
 
-  form.parse(req, function(err, fields, files) {
-    if (err) return res.send({error : err.message})
-
-    console.log(`fields = ${JSON.stringify(fields, null, 2)}`);
-    console.log(`files = ${JSON.stringify(files, null, 2)}`);
-
-    const imagePath = files.image[0].path;
-    const imageFileName = imagePath.slice(imagePath.lastIndex("\\") + 1);
-    const imageUrl = imgBaseUrl + imageFileName;
-    console.log(imageUrl);
-  })
-});
-
+const Note = mongoose.model("Note", schema);
+app.use(bodyParser.urlencoded({extended: true}));
 
 //running
 app.listen(port, () => {
