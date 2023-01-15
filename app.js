9dd289c03708fff189ad844336b21b868b964b65
km5762 <kumyles@gmail.com>
2023-01-14 21:17:09 -0500

    Add DB query function and public assets folder

--- app.js
+++ app.js
@@ -6,14 +6,31 @@ const ejs = require('ejs');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const port = 3000;
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
 //serve static pages. index and upload will stay as is. spots will be dynamic from server
 const password = fs.readFileSync("./.env.txt", "utf-8").toString();
 
-mongoose.connect(`mongodb+srv://sampleGoat:${password}@cluster0.6vhvr.mongodb.net/?retryWrites=true&w=majority`)
-// mongoose.connect(`mongodb+srv://sampleGoat:${password}@cluster0.6vhvr.mongodb.net/GoatHacks`)
+// connect to mongoosedb
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://sampleGoat:${password}@cluster0.6vhvr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+async function queryAll() {
+  try {
+    const database = client.db('spotsDB');
+    const studyspots = database.collection('studyspots');
+    const query = { location : 'Higgins'};
+    const spot = await studyspots.find(query);
+    console.log(spot);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
 const schema = {
   location: String,
   area: String,
@@ -22,25 +39,26 @@ const schema = {
   img: String
 }
 
+const Note = mongoose.model("Note", schema);
+const Spot = mongoose.model("studyspots", schema);
+
+app.use('/public', express.static('public'));
+
 app.get('/', (request, response) => {
-  app.use(express.static(__dirname + '/static'))
-  response.sendFile(__dirname + '/static/index.html');
+  response.sendFile(__dirname + '/public/index.html');
 })
 
 app.get('/upload.html', (request, response) => {
-  app.use(express.static(__dirname + '/static'))
-  response.sendFile(__dirname + '/static/upload.html');
+  response.sendFile(__dirname + '/public/upload.html');
 })
 
-app.get('/views/test.ejs', (request, response) => {
-  let name = 'myles';
-  response.render(__dirname + '/views/test.ejs', {
-      userName: name
-  });
+app.get('/views/spots.ejs', (request, response) => {
+  Spot.find({}, function(err, spots) {
+    response.render('spots', {
+      spotsList: spots
+    })
+  })
 })
-
-const Note = mongoose.model("Note", schema);
-app.use(bodyParser.urlencoded({extended: true}));
 
 //running
 app.listen(port, function() {
