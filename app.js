00635be96286b21d36154aac4f6d3cfbf159b680
km5762 <kumyles@gmail.com>
2023-01-15 02:37:40 -0500

    Add cards display + spacing

--- app.js
+++ app.js
@@ -88,17 +88,19 @@ app.get('/upload.html', (request, response) => {
 })
 
 app.get('/views/spots.ejs', (request, response) => {
-  MongoClient.connect(url, (err, db) => {
-    if (err) throw err;
-    var dbo = db.db("spotsDB");
-    var query = {};
-    dbo.collection("studyspots").find(query).toArray(function (err, result) {
-      if (err) throw err;
-      console.log(result);
-      db.close();
-      response.render('spots', { spotList: result });
-    });
-  });
+  // MongoClient.connect(url, (err, db) => {
+  //   if (err) throw err;
+  //   var dbo = db.db("spotsDB");
+  //   var query = {};
+  //   dbo.collection("studyspots").find(query).toArray(function (err, result) {
+  //     if (err) throw err;
+  //     console.log(result);
+  //     db.close();
+  //     response.render('spots', { spotList: result });
+  //   });
+  // });
+  list = [1,2,3,4,5];
+  response.render('spots', {list : list});
 });
 
 app.post('/upload.html', multer(multerConfig).any(), async (req, res) => {
