6bca8e3c261c647542af64a42ba3cf27dbbed976
km5762 <kumyles@gmail.com>
2023-01-15 06:59:18 -0500

    Center star display

--- app.js
+++ app.js
@@ -34,21 +34,6 @@ const multerConfig = {
   }
 }
 
-async function queryAll() {
-  const client = new MongoClient(url, mongoConfig);
-  try {
-    const database = client.db('spotsDB');
-    const studyspots = database.collection('studyspots');
-    const query = {};
-    const spot = await studyspots.find(query).toArray();
-    console.log(spot);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-// queryAll().catch(console.dir);
-
 app.use('/public', express.static('public'));
 
 //TODO this serves images to validate
@@ -69,7 +54,6 @@ app.get('/views/spots.ejs', (request, response) => {
     var query = {};
     dbo.collection("studyspots").find(query).toArray(function (err, result) {
       if (err) throw err;
-      console.log(result);
       db.close();
       response.render('spots', { spotList: result });
     });
